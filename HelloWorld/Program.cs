8bb27bb01bd3b9956a34f11c2a9bb9a6ab3b59dc
Braxten Crowell <bcrowell3508@gmail.com>
2026-09-09 14:19:03 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Braxten Crowell
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Braxten Crowell.");
+Console.WriteLine("This is my first program at ETSU.");
